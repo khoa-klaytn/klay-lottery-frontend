eb9fb74fe9d1321a9ec9543a068b87c2bdb75908
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-11 17:36:58 +0800

    feat(MakeLotteryClaimable): if testnet, show input to set final number

--- apps/web/src/views/Lottery/Operator/MakeLotteryClaimable.tsx
+++ apps/web/src/views/Lottery/Operator/MakeLotteryClaimable.tsx
@@ -1,30 +1,44 @@
 import * as chains from 'config/chains'
 import { Button } from '@pancakeswap/uikit'
 import { LotteryStatus } from 'config/constants/types'
-import { useCallback, useMemo } from 'react'
+import { FormEvent, useCallback, useMemo, useState } from 'react'
 import { Chain, useChainId } from 'wagmi'
 import { SendTransactionResult } from 'wagmi/dist/actions'
 
 export default function MakeLotteryClaimable({ callWithGasPrice, lotteryContract, lotteryId, status }) {
   const chainId = useChainId()
   const chain = useMemo(() => chains[chains.CHAIN_QUERY_NAME[chainId]] as Chain, [chainId])
-  const makeLotteryClaimable = useCallback(async () => {
-    let res: SendTransactionResult
-    if (chain.testnet) {
-      res = await callWithGasPrice(lotteryContract, 'setFinalNumberAndMakeLotteryClaimable', [
-        BigInt(lotteryId),
-        true,
-        BigInt(1),
-      ])
-    } else {
-      res = await callWithGasPrice(lotteryContract, 'drawFinalNumberAndMakeLotteryClaimable', [BigInt(lotteryId), true])
-    }
-    console.log(res)
-  }, [chain, callWithGasPrice, lotteryContract, lotteryId])
+  const [finalNumber, setFinalNumber] = useState(0)
+  const makeLotteryClaimable = useCallback(
+    async (ev: FormEvent<HTMLFormElement>) => {
+      ev.preventDefault()
+
+      let res: SendTransactionResult
+      if (chain.testnet) {
+        res = await callWithGasPrice(lotteryContract, 'setFinalNumberAndMakeLotteryClaimable', [
+          BigInt(lotteryId),
+          true,
+          BigInt(finalNumber),
+        ])
+      } else {
+        res = await callWithGasPrice(lotteryContract, 'drawFinalNumberAndMakeLotteryClaimable', [
+          BigInt(lotteryId),
+          true,
+        ])
+      }
+      console.log(res)
+    },
+    [chain, callWithGasPrice, lotteryContract, lotteryId, finalNumber],
+  )
 
   return (
-    <Button type="button" onClick={makeLotteryClaimable} disabled={status === LotteryStatus.CLOSE}>
-      Make Lottery Claimable
-    </Button>
+    <form onSubmit={makeLotteryClaimable}>
+      {chain.testnet && (
+        <input type="number" value={finalNumber} onChange={(ev) => setFinalNumber(ev.target.valueAsNumber)} />
+      )}
+      <Button type="submit" disabled={status !== LotteryStatus.CLOSE}>
+        Make Lottery Claimable
+      </Button>
+    </form>
   )
 }
