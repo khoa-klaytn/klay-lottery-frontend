fac207a8dd5b75b014c98944a2e60637868e3100
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-27 17:00:58 +0800

    refactor(PhishingBanner): hide for now

--- packages/utils/user/phishingBanner.ts
+++ packages/utils/user/phishingBanner.ts
@@ -1,19 +1,20 @@
 import { atom, useAtom } from 'jotai'
-import differenceInDays from 'date-fns/differenceInDays'
+// import differenceInDays from 'date-fns/differenceInDays'
 import { atomWithStorage } from 'jotai/utils'
 
 const phishingBannerAtom = atomWithStorage<number>('pcs:phishing-banner', 0)
 
-const hidePhishingBannerAtom = atom(
-  (get) => {
-    const now = Date.now()
-    const last = get(phishingBannerAtom)
-    const notPreview = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'preview'
-    return last ? differenceInDays(now, last) >= 1 && notPreview : notPreview
+const showPhishingBannerAtom = atom(
+  (/* get */) => {
+    // const now = Date.now()
+    // const last = get(phishingBannerAtom)
+    // const notPreview = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'preview'
+    // return last ? differenceInDays(now, last) >= 1 && notPreview : notPreview
+    return false // TODO: uncomment once deployed on Klaytn Mainnet
   },
   (_, set) => set(phishingBannerAtom, Date.now()),
 )
 
 export function usePhishingBanner() {
-  return useAtom(hidePhishingBannerAtom)
+  return useAtom(showPhishingBannerAtom)
 }
