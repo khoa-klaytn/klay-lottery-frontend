f825dffa5f2d78dd8babf4d78667c7d6e0647cd4
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-30 16:31:47 +0800

    refactor(Info dropdown): hide for now

--- apps/web/src/components/Menu/config/config.ts
+++ apps/web/src/components/Menu/config/config.ts
@@ -1,6 +1,6 @@
 import { ContextApi } from '@pancakeswap/localization'
-import { DropdownMenuItemType, DropdownMenuItems, MenuItemsType, MoreIcon } from '@pancakeswap/uikit'
-import { SUPPORT_ONLY_KLAYTN } from 'config/constants/supportChains'
+import { /* DropdownMenuItemType, */ DropdownMenuItems, MenuItemsType /* MoreIcon */ } from '@pancakeswap/uikit'
+// import { SUPPORT_ONLY_KLAYTN } from 'config/constants/supportChains'
 
 export type ConfigMenuDropDownItemsType = DropdownMenuItems & { hideSubNav?: boolean }
 export type ConfigMenuItemsType = Omit<MenuItemsType, 'items'> & { hideSubNav?: boolean; image?: string } & {
@@ -33,50 +33,50 @@ const config: (
       href: '/lottery',
       items: [],
     },
-    {
-      label: '',
-      href: '/info',
-      icon: MoreIcon,
-      hideSubNav: true,
-      items: [
-        {
-          label: t('Info'),
-          href: '/info/v3',
-        },
-        {
-          label: t('Affiliate Program'),
-          href: '/affiliates-program',
-        },
-        {
-          label: t('Voting'),
-          href: '/voting',
-          supportChainIds: SUPPORT_ONLY_KLAYTN,
-          image: '/images/voting/voting-bunny.png',
-        },
-        {
-          type: DropdownMenuItemType.DIVIDER,
-        },
-        {
-          label: t('Leaderboard'),
-          href: '/teams',
-          supportChainIds: SUPPORT_ONLY_KLAYTN,
-          image: '/images/decorations/leaderboard.png',
-        },
-        {
-          type: DropdownMenuItemType.DIVIDER,
-        },
-        {
-          label: t('Blog'),
-          href: 'https://blog.pancakeswap.finance',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-        },
-        {
-          label: t('Docs'),
-          href: 'https://docs.pancakeswap.finance',
-          type: DropdownMenuItemType.EXTERNAL_LINK,
-        },
-      ].map((item) => addMenuItemSupported(item, chainId)),
-    },
+    // {
+    //   label: '',
+    //   href: '/info',
+    //   icon: MoreIcon,
+    //   hideSubNav: true,
+    //   items: [
+    //     {
+    //       label: t('Info'),
+    //       href: '/info/v3',
+    //     },
+    //     {
+    //       label: t('Affiliate Program'),
+    //       href: '/affiliates-program',
+    //     },
+    //     {
+    //       label: t('Voting'),
+    //       href: '/voting',
+    //       supportChainIds: SUPPORT_ONLY_KLAYTN,
+    //       image: '/images/voting/voting-bunny.png',
+    //     },
+    //     {
+    //       type: DropdownMenuItemType.DIVIDER,
+    //     },
+    //     {
+    //       label: t('Leaderboard'),
+    //       href: '/teams',
+    //       supportChainIds: SUPPORT_ONLY_KLAYTN,
+    //       image: '/images/decorations/leaderboard.png',
+    //     },
+    //     {
+    //       type: DropdownMenuItemType.DIVIDER,
+    //     },
+    //     {
+    //       label: t('Blog'),
+    //       href: 'https://blog.pancakeswap.finance',
+    //       type: DropdownMenuItemType.EXTERNAL_LINK,
+    //     },
+    //     {
+    //       label: t('Docs'),
+    //       href: 'https://docs.pancakeswap.finance',
+    //       type: DropdownMenuItemType.EXTERNAL_LINK,
+    //     },
+    //   ].map((item) => addMenuItemSupported(item, chainId)),
+    // },
   ].map((item) => addMenuItemSupported(item, chainId))
 
 export default config
