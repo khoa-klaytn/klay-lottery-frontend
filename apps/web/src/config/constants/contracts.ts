db4a68ac26443b370ac75f25fec5928deaa173df
khoa-klaytn <mk.tran@klaytn.foundation>
2023-10-31 17:10:30 +0800

    chore(klayLottery): update abi & address

--- apps/web/src/config/constants/contracts.ts
+++ apps/web/src/config/constants/contracts.ts
@@ -17,7 +17,7 @@ export default {
     [ChainId.BSC]: '0x6Ab8463a4185b80905E05A9ff80A2d6b714B9e95',
   },
   klayLottery: {
-    [ChainId.KLAYTN_TESTNET]: '0x72ac202C51D5744A691146658a0AbC971271a653',
+    [ChainId.KLAYTN_TESTNET]: '0xa670cBDCea52d444cD39B0aC0Aa682539660db8E',
     [ChainId.KLAYTN]: '0x', // TODO: deploy to mainnet
   },
   multiCall: {
