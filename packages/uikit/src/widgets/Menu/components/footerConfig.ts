4e18da8c83669f0f3a220449e3c0e0b98d2b6b8b
khoa-klaytn <mk.tran@klaytn.foundation>
2023-11-16 13:14:06 +0800

    refactor(footerConfig): comment all but Ecosystem

--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -11,59 +11,59 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
       },
     ],
   },
-  {
-    label: t("Developers"),
-    items: [
-      {
-        label: t("Contributing"),
-        href: "https://docs.sweepstakes.finance/developers/contributing",
-      },
-      {
-        label: t("Github"),
-        href: "https://github.com/sweepstakes",
-      },
-      {
-        label: t("Bug Bounty"),
-        href: "https://docs.sweepstakes.finance/developers/bug-bounty",
-      },
-    ],
-  },
-  {
-    label: t("Support"),
-    items: [
-      {
-        label: t("Contact"),
-        href: "https://docs.sweepstakes.finance/contact-us/customer-support",
-      },
-      {
-        label: t("Troubleshooting"),
-        href: "https://docs.sweepstakes.finance/readme/help/troubleshooting",
-      },
-      {
-        label: t("Documentation"),
-        href: "https://docs.sweepstakes.finance/",
-      },
-    ],
-  },
-  {
-    label: t("About"),
-    items: [
-      {
-        label: t("Terms Of Service"),
-        href: "https://sweepstakes.finance/terms-of-service",
-      },
-      {
-        label: t("Blog"),
-        href: "https://blog.sweepstakes.finance/",
-      },
-      {
-        label: t("Brand Assets"),
-        href: "https://docs.sweepstakes.finance/ecosystem-and-partnerships/brand",
-      },
-      {
-        label: t("Careers"),
-        href: "https://docs.sweepstakes.finance/team/become-a-chef",
-      },
-    ],
-  },
+  // {
+  //   label: t("Developers"),
+  //   items: [
+  //     {
+  //       label: t("Contributing"),
+  //       href: "https://docs.sweepstakes.finance/developers/contributing",
+  //     },
+  //     {
+  //       label: t("Github"),
+  //       href: "https://github.com/sweepstakes",
+  //     },
+  //     {
+  //       label: t("Bug Bounty"),
+  //       href: "https://docs.sweepstakes.finance/developers/bug-bounty",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: t("Support"),
+  //   items: [
+  //     {
+  //       label: t("Contact"),
+  //       href: "https://docs.sweepstakes.finance/contact-us/customer-support",
+  //     },
+  //     {
+  //       label: t("Troubleshooting"),
+  //       href: "https://docs.sweepstakes.finance/readme/help/troubleshooting",
+  //     },
+  //     {
+  //       label: t("Documentation"),
+  //       href: "https://docs.sweepstakes.finance/",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: t("About"),
+  //   items: [
+  //     {
+  //       label: t("Terms Of Service"),
+  //       href: "https://sweepstakes.finance/terms-of-service",
+  //     },
+  //     {
+  //       label: t("Blog"),
+  //       href: "https://blog.sweepstakes.finance/",
+  //     },
+  //     {
+  //       label: t("Brand Assets"),
+  //       href: "https://docs.sweepstakes.finance/ecosystem-and-partnerships/brand",
+  //     },
+  //     {
+  //       label: t("Careers"),
+  //       href: "https://docs.sweepstakes.finance/team/become-a-chef",
+  //     },
+  //   ],
+  // },
 ];
