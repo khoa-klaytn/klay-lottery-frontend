e8b130beb7fdcfe2a2bdca9601a374a5b1b996ca
khoa-klaytn <mk.tran@klaytn.foundation>
2023-11-29 10:50:57 +0800

    refactor(useGetNextLotteryEvent): hardcode next event time to UTC 0200

--- apps/web/src/views/Lottery/hooks/useGetNextLotteryEvent.ts
+++ apps/web/src/views/Lottery/hooks/useGetNextLotteryEvent.ts
@@ -8,38 +8,40 @@ interface LotteryEvent {
   preCountdownText?: string
 }
 
-const vrfRequestTime = 180 // 3 mins
-const secondsBetweenRounds = 300 // 5 mins
-const transactionResolvingBuffer = 30 // Delay countdown by 30s to ensure contract transactions have been calculated and broadcast
+const vrfRequestTime = 15
+const transactionResolvingBuffer = 5 // Delay countdown to ensure contract transactions have been calculated and broadcast
 
 const useGetNextLotteryEvent = (endTime: number, status: LotteryStatus): LotteryEvent => {
   const { t } = useTranslation()
   return useMemo(() => {
-    // Current lottery is active
-    if (status === LotteryStatus.OPEN) {
-      return {
-        nextEventTime: endTime + transactionResolvingBuffer,
-        preCountdownText: null,
-        postCountdownText: t('until the draw'),
-      }
+    switch (status) {
+      case LotteryStatus.OPEN:
+        return {
+          nextEventTime: endTime + transactionResolvingBuffer,
+          preCountdownText: null,
+          postCountdownText: t('until the draw'),
+        }
+      case LotteryStatus.CLOSE:
+        return {
+          nextEventTime: endTime + transactionResolvingBuffer + vrfRequestTime,
+          preCountdownText: t('Winners announced in'),
+          postCountdownText: null,
+        }
+      case LotteryStatus.CLAIMABLE:
+        const nextEventTime = new Date()
+        nextEventTime.setUTCHours(2, 0, 0, 0)
+        const time = nextEventTime.getTime()
+        if (time < Date.now()) {
+          return { nextEventTime: null, preCountdownText: null, postCountdownText: null }
+        }
+        return {
+          nextEventTime: time / 1000,
+          preCountdownText: t('Tickets on sale in'),
+          postCountdownText: null,
+        }
+      default:
+        return { nextEventTime: null, preCountdownText: null, postCountdownText: null }
     }
-    // Current lottery has finished but not yet claimable
-    if (status === LotteryStatus.CLOSE) {
-      return {
-        nextEventTime: endTime + transactionResolvingBuffer + vrfRequestTime,
-        preCountdownText: t('Winners announced in'),
-        postCountdownText: null,
-      }
-    }
-    // Current lottery claimable. Next lottery has not yet started
-    if (status === LotteryStatus.CLAIMABLE) {
-      return {
-        nextEventTime: endTime + transactionResolvingBuffer + secondsBetweenRounds,
-        preCountdownText: t('Tickets on sale in'),
-        postCountdownText: null,
-      }
-    }
-    return { nextEventTime: null, preCountdownText: null, postCountdownText: null }
   }, [endTime, status, t])
 }
 
